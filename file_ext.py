a927d1fd36c8091ab94a5eade72451ed6fe1d3c1
Sayan371 <113833986+Sayan371@users.noreply.github.com>
2024-08-10 20:30:40 +0530

    Add files via upload

---
+++ file_ext.py
@@ -0,0 +1,19 @@
+video_file_formats = ["mp4", "mkv", "flv", "avi", "mov", "wmv", "avchd", "webm", "mpeg", "mpg", "m4v", "3gp", "3g2", "f4v", "f4p", "f4a", "f4b", "ogv", "ogm", "vob", "ts", "m2ts", "mts", "rm", "rmvb", "asf", "mxf", "divx", "xvid", "qt", "mpe", "mpv", "m2v"]
+
+audio_file_formats = ["mp3", "wav", "aac", "flac", "alac", "ogg", "wma", "aiff", "pcm", "m4a", "amr", "dsd", "mqa", "opus", "ra", "au", "vox", "mid", "midi", "ape", "wv", "mpc", "tta", "act", "dss", "dvf", "gsm", "iklax", "ivs", "m4p", "mmf", "mpc", "msv", "rm", "sln", "raw", "webm", "3gp", "8svx"]
+
+image_file_formats = ["jpg", "jpeg", "png", "gif", "bmp", "tiff", "tif", "webp", "svg", "heic", "heif", "raw", "cr2", "nef", "orf", "sr2", "arw", "dng"]
+
+document_file_formats =[
+    "doc", "docx", "pdf", "txt", "rtf", "odt", "html", "htm", "ppt", "pptx", "xls", "xlsx", "csv", "epub", "mobi", "pages", "key", "numbers", "md", "tex", "accd", "odb", "accdt", "hoop"]
+
+archive_file_formats = [
+    "zip", "rar", "7z", "tar", "gz", "bz2", "xz", "lz", "lzma", "arj",
+    "cab", "iso", "dmg", "targz", "tgz", "tarbz2", "tbz2", "tarxz", 
+    "txz", "tarlz", "tlz", "tarlzma", "tlzma", "z", "zst", "zpaq", 
+    "ace", "alz", "bzip", "cpio", "cbr", "cbz", "chm", "chz", "dmg",
+    "hfs", "iso", "jar", "kdbx", "lzh", "lzx", "pax", "r00", "r01", 
+    "r02", "r03", "r04", "r05", "r06", "r07", "r08", "r09", "r10",
+    "sda", "sdc", "tarlz4", "tlz4", "tarzst", "tzst", "zipx"
+]
+all_file_formats = video_file_formats + audio_file_formats + image_file_formats
